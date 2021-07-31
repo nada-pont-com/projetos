9d59a64199ab112db61777644eb5f3c99c04c037
nada <nada.pont.com@gmail.com>
2021-07-31 18:01:09 -0300

    ative 2

--- fac/shell script/ative2/ismael_vinicius_EstadoES.sh
+++ fac/shell script/ative2/ismael_vinicius_EstadoES.sh
@@ -19,7 +19,7 @@ qtdProcessador=$(lscpu -pCPU,Core,Socket | egrep -v '^#' | sort -u -t, -k 3 | wc
 qtdNucleos=$(lscpu -pCPU,Core,Socket | egrep -v '^#' | sort -u -t, -k 2,3 | wc -l)
 
 # - Qtde. total RAM
-totalRam=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+totalRam=$(cat /proc/meminfo | grep MemTotal | awk '{print $2/1048576" GB"}' | tr "," ".")
 
 # - Qtde de Hubs USB
 qtdUSB=$(lsusb | wc -l)
@@ -28,25 +28,33 @@ qtdUSB=$(lsusb | wc -l)
 
 
 # - Modelo Placa de rede Ethernet/WiFi (a que estiver em uso)
-placaNet=$(lspci | grep Ethernet | cut -f3 -d":")
+placaNet=$(lspci | grep Ethernet | cut -f3 -d":" | tr "\n" " | ")
 
 # - Velocidade Ethernet/WiFi (a que estiver em uso)
 
 # - Quantidade de discos (HD/SSD)
 qtdDisco=$(lsblk -o SIZE,TYPE | grep disk | wc -l)
 
+sum=0
+
 # - Capacidade HD/SSD (Gb)
-capacidade=$(lsblk -o SIZE,TYPE | grep disk | cut -f3 -d" ")
+capacidade=$(lsblk -b -io SIZE,TYPE | grep disk | awk 'BEGIN{OFS="\t"} {print $1/1073741824;}'| awk '{ sum += $1 } END { print sum" GB" }')
+
+# capacidade=0
+# tam=$(wc -l <<< $capacidades)
+
+# for ((i=0;i<$tam;i++)); do
+
+# done
 
 # - Modelo Placa Vídeo
-placaVideo=$(lspci | grep VGA |cut -f3 -d":")
+placaVideo=$(lspci | grep VGA |cut -f3 -d":" | tr "\n" " ")
 
 # ismael_vinicius__${data}EstadoES${user}.csv
 
 FILE=ismael_vinicius__${data}EstadoES${user}.csv
 
 if [[ ! -e "$FILE" ]]; then 
-    echo "$FILE not exist" 
     echo "Data (AAAAMMDD-HH:MM:SS), Usuário corrente, Nome do host, Qtde. de Processadores, Qtde de núcleos, Qtde. total RAM, Qtde de Hubs USB,  Qtde. USB Ativa, Modelo Placa de rede Ethernet/WiFi, Velocidade Ethernet/WiFi, Quantidade de discos, Capacidade HD/SSD, Modelo Placa Vídeo" >> $FILE
 fi
 
